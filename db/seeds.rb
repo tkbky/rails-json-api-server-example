18e4b7bae440ac2838a1a978c6021d4f6d5a9bf5
tkbky <kheryee.ting@tinkerbox.com.sg>
2016-06-13 12:27:01 +0800

    add model & seeds for Post

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+if Post.count == 0
+  365.times { |i| Post.create!(title: "#{Date.today + i.day}", body: "Today is #{(Date.today + i.day).strftime('%A')}")}
+end
