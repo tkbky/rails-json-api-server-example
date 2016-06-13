ce7c4631a57c70606dd768b9f84d84e786e1d358
tkbky <kheryee.ting@tinkerbox.com.sg>
2016-06-13 15:53:42 +0800

    add UserSerializer & seeds for User

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
 if Post.count == 0
   365.times { |i| Post.create!(title: "#{Date.today + i.day}", body: "Today is #{(Date.today + i.day).strftime('%A')}")}
 end
+
+if User.count == 0
+  User.create!(email: 'user@example.com', password: '123123123', password_confirmation: '123123123')
+end
